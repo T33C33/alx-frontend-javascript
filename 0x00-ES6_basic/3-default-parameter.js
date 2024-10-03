9ce36e3121efd3b1197fdbbd4ad06b47b3484b83
T33C33 <teeceeiheukwumere@gmail.com>
2024-10-03 15:31:31 +0100

    3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,7 @@
 export default function getSumOfHoods(
   initialNumber,
-  expansion1989 = 89,
-  expansion2019 = 19,
+  expansion1989 = undefined,
+  expansion2019 = undefined,
 ) {
   if (expansion1989 === undefined) {
     expansion1989 = 89;
