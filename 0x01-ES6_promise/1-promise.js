b8cbb6903e218bb5feb1b47d688f038d952b07d2
T33C33 <teeceeiheukwumere@gmail.com>
2024-10-14 14:49:55 +0100

    2-t.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,24 +3,10 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({
         status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
-}
-
-//Ozioma's code
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
         body: "Success",
       });
     } else {
-      reject(new error("The fake API is not working currently"));
+      reject(new Error("The fake API is not working currently"));
     }
   });
 }
