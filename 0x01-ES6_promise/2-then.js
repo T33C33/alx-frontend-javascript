1b288f47590fa3a85f0e2ccae8081f24aa663e39
T33C33 <teeceeiheukwumere@gmail.com>
2024-10-14 13:27:36 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,17 @@
-export default function handleResponseFromAPI(promise) {
+/* export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: 'success' }))
+    .then(() => ({ status: 200, body: "success" }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log("Got a response from the API");
+    });
+} */
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
