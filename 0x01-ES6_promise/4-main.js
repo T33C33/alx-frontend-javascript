64d7de7fe0d5cbe9d4dbc19a21a4b3a0ca40498e
T33C33 <teeceeiheukwumere@gmail.com>
2024-10-15 12:10:26 +0100

    4-user-promise.js

---
+++ 0x01-ES6_promise/4-main.js
@@ -0,0 +1,10 @@
+import signUpUser from "./4-user-promise";
+
+console.log(signUpUser("Bob", "Dylan"));
+
+/*{
+  firstName: value,
+  lastName: value,
+}*/
+
+// output is Promise { { firstName: 'Bob', lastName: 'Dylan' } }
