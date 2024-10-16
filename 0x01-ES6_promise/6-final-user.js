21fee867e16a616e96def4dea1e0d9d27f5e9aac
T33C33 <teeceeiheukwumere@gmail.com>
2024-10-16 11:33:03 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,22 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
+  ]).then((results) =>
+    results.map((result) => {
+      if (result.status === "fulfilled") {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
       return {
         status: result.status,
-        value: result.value,
+        value: result.reason.toString(),
       };
-    }
-    return {
-      status: result.status,
-      value: result.reason,
-    };
-  }));
+    })
+  );
 }
