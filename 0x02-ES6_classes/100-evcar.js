9d24e3c42708906c62548f6214ef3f796386abc0
T33C33 <teeceeiheukwumere@gmail.com>
2024-10-18 15:31:14 +0100

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,19 +1,21 @@
-export default class Car {
-  constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
   }
 
   // Clone method
   cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
     // Clone method that returns a new instance of the same class
-    const Species = this.constructor[Symbol.species];
-    return new Species(this._brand, this._motor, this._color);
+    /* const Species = this.constructor[Symbol.species];
+    return new Species(this._brand, this._motor, this._color); */
   }
 
   // Static method for Symbol.species
-  static get [Symbol.species]() {
+  /* static get [Symbol.species]() {
     return this; // By default, return the constructor of the current class
-  }
+  } */
 }
--- 0x02-ES6_classes/100-main.js
+++ 0x02-ES6_classes/100-main.js
@@ -5,3 +5,17 @@ console.log(ec1);
 
 const ec2 = ec1.cloneCar();
 console.log(ec2);
+
+/*
+Import Car from 10-car.js.
+
+Implement a class named EVCar that extends the Car class:
+
+    Constructor attributes:
+        brand (String)
+        motor (String)
+        color (String)
+        range (String)
+    Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+    For privacy reasons, when cloneCar is called on a EVCar object, the object returned should be an instance of Car instead of EVCar.
+*/
